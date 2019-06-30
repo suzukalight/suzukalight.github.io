07f6f459bd46664597b306eecea269b1f0760464
suzukalight <mail@mkubara.com>
2019-06-30 15:26:06 +0900

    Update page contents

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-// Utilities
+import { Link, graphql } from 'gatsby';
 import kebabCase from 'lodash/kebabCase';
 
-// Components
-import { Helmet } from 'react-helmet';
-import { Link, graphql } from 'gatsby';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const TagsPage = ({
+  location,
   data: {
     allMarkdownRemark: { group },
     site: {
@@ -16,39 +15,24 @@ const TagsPage = ({
     },
   },
 }) => (
-  <div>
-    <Helmet title={title} />
-    <div>
-      <h1>Tags</h1>
-      <ul>
-        {group.map(tag => (
-          <li key={tag.fieldValue}>
-            <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-              {tag.fieldValue} ({tag.totalCount})
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
+  <Layout location={location} title={title}>
+    <SEO title="Tags" />
+    <h1>Tags</h1>
+    <ul>
+      {group.map(tag => (
+        <li key={tag.fieldValue}>
+          <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+            {tag.fieldValue} ({tag.totalCount})
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
 TagsPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      group: PropTypes.arrayOf(
-        PropTypes.shape({
-          fieldValue: PropTypes.string.isRequired,
-          totalCount: PropTypes.number.isRequired,
-        }).isRequired,
-      ),
-    }),
-    site: PropTypes.shape({
-      siteMetadata: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-      }),
-    }),
-  }),
+  location: PropTypes.object,
+  data: PropTypes.object,
 };
 
 export default TagsPage;
--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,60 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-// Components
 import { Link, graphql } from 'gatsby';
 
-const Tags = ({ pageContext, data }) => {
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Tags = ({ location, pageContext, data }) => {
   const { tag } = pageContext;
   const { edges, totalCount } = data.allMarkdownRemark;
   const tagHeader = `${totalCount} post${totalCount === 1 ? '' : 's'} tagged with "${tag}"`;
+  const siteTitle = data.site.siteMetadata.title;
 
   return (
-    <div>
+    <Layout location={location} title={siteTitle}>
+      <SEO title={tagHeader} />
       <h1>{tagHeader}</h1>
       <ul>
-        {edges.map(({ node }) => {
-          const { slug } = node.fields;
-          const { title } = node.frontmatter;
-          return (
-            <li key={slug}>
-              <Link to={slug}>{title}</Link>
-            </li>
-          );
-        })}
+        {edges.map(({ node: { fields: { slug }, frontmatter: { title } } }) => (
+          <li key={slug}>
+            <Link to={slug}>{title}</Link>
+          </li>
+        ))}
       </ul>
-      <Link to="/tags">All tags</Link>
-    </div>
+      <p>
+        <Link to="/tags">All tags</Link>
+      </p>
+    </Layout>
   );
 };
 
 Tags.propTypes = {
-  pageContext: PropTypes.shape({
-    tag: PropTypes.string.isRequired,
-  }),
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      totalCount: PropTypes.number.isRequired,
-      edges: PropTypes.arrayOf(
-        PropTypes.shape({
-          node: PropTypes.shape({
-            frontmatter: PropTypes.shape({
-              title: PropTypes.string.isRequired,
-            }),
-            fields: PropTypes.shape({
-              slug: PropTypes.string.isRequired,
-            }),
-          }),
-        }).isRequired,
-      ),
-    }),
-  }),
+  pageContext: PropTypes.object,
+  data: PropTypes.object,
 };
 
 export default Tags;
 
 export const pageQuery = graphql`
   query($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
       limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
