12713be37c516653debb04426f96a5a7c270a883
suzukalight <mail@mkubara.com>
2019-06-30 20:07:12 +0900

    Fix styles

--- src/components/molecules/Bio/index.js
+++ src/components/molecules/Bio/index.js
@@ -2,29 +2,32 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 
+import { rhythm } from '../../../utils/typography';
+
 import styles from './index.module.scss';
 
-const Bio = () => {
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      avatar: file(absolutePath: { regex: "/tarako.jpg/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          author
-          social {
-            twitter
-          }
+const query = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/tarako.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
-  `);
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`;
 
+const Bio = () => {
+  const data = useStaticQuery(query);
   const { author, social } = data.site.siteMetadata;
 
   return (
@@ -32,8 +35,15 @@ const Bio = () => {
       <Image
         fixed={data.avatar.childImageSharp.fixed}
         alt={author}
-        style={{}}
-        imgStyle={{}}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          minWidth: 50,
+          borderRadius: `100%`,
+        }}
+        imgStyle={{
+          borderRadius: `50%`,
+        }}
         // style={styles.heroImageWrap}
         // imgStyle={styles.heroImage}
       />
