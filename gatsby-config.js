5d8763ccbbd83e1665230db24fae3ec2ea16688e
suzukalight <mail@mkubara.com>
2019-06-30 23:42:51 +0900

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,18 +74,6 @@ module.exports = {
     },
     `gatsby-plugin-feed`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'suzukalight.com',
