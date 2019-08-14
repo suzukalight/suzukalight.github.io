e90d60277cc002dd88c29c5deaacd7f4713df4ca
suzukalight <mail@mkubara.com>
2019-08-14 12:17:21 +0900

    Split source of blog post

---
+++ src/components/templates/BlogPost/source-contentful.js
@@ -0,0 +1,38 @@
+import { graphql } from 'gatsby';
+
+import BlogPost from './';
+
+export default BlogPost;
+
+export const pageQuery = graphql`
+  query ContentfulBlogPostBySlug($id: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    contentfulArticle(id: { eq: $id }) {
+      __typename
+      id
+      slug
+      title
+      body {
+        json
+      }
+      date
+      category
+      tags
+      # description
+      # tableOfContents
+      # hero {
+      #   fixed(width: 1440) {
+      #     width
+      #     height
+      #     src
+      #     srcSet
+      #   }
+      # }
+    }
+  }
+`;
