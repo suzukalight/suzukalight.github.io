47a0e33fc35b88686889a7ea4adda4723ac1c630
suzukalight <mail@mkubara.com>
2019-08-15 11:36:05 +0900

    Support code block and paragraph

--- src/utils/contentful.jsx
+++ src/utils/contentful.jsx
@@ -4,8 +4,12 @@ import { BLOCKS } from '@contentful/rich-text-types';
 
 const renderOptions = {
   renderNode: {
+    [BLOCKS.PARAGRAPH]: (_, children) => <p className="contentful-paragraph">{children}</p>,
     [BLOCKS.EMBEDDED_ASSET]: node => {
-      const { title, description, file } = node.data.target.fields;
+      const { target } = node.data || {};
+      const { title, description, file } = (target || {}).fields || {};
+      if (!file) return null;
+
       const key1 = Object.keys(file)[0];
       const { contentType, url } = file[key1];
       const mimeGroup = contentType.split('/')[0];
