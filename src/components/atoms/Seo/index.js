c63a1ae394bd93062acef5777ea3a6d792bd6dd1
suzukalight <mail@mkubara.com>
2019-09-27 22:36:52 +0900

    Fix

--- src/components/atoms/Seo/index.js
+++ src/components/atoms/Seo/index.js
@@ -10,6 +10,7 @@ const query = graphql`
         title
         description
         author
+        image
       }
     }
   }
@@ -20,14 +21,17 @@ function Seo({ description, lang, meta, title }) {
   const { siteMetadata } = data.site;
   const metaDescription = description || siteMetadata.description;
   const metaData = [
+    { name: `image`, content: siteMetadata.image },
     { name: `description`, content: metaDescription },
     { property: `og:title`, content: title },
     { property: `og:description`, content: metaDescription },
     { property: `og:type`, content: `website` },
+    { property: `og:image`, content: siteMetadata.image },
     { name: `twitter:card`, content: `summary` },
     { name: `twitter:creator`, content: siteMetadata.author },
     { name: `twitter:title`, content: title },
     { name: `twitter:description`, content: metaDescription },
+    { name: `twitter:image`, content: siteMetadata.image },
   ].concat(meta);
 
   return (
